43d28f667e79a03024db1a84a38e1188af4ca784
GustavoJaya <bryanjaya06@yahoo.com>
2023-10-01 15:25:50 -0500

    primer commit

---
+++ mongo.py
@@ -0,0 +1,25 @@
+
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+
+
+uri = f"mongodb+srv://{user}:{password}@cluster0.pqm6abd.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+client .get_database('prueba').get_collection('BJ').insert_one(document={"marca":"opel","modelo":"omega"})
+
+client .get_database('prueba').get_collection('Final').insert_one(document={"Tiempo":"soleado","Estación":"Verano"})
